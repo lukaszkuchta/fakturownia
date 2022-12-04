d373e320236514b40bd1b847379cbcf20348261c
lukaszkuchta <lukaszkuchta10@gmail.com>
2022-12-04 12:04:05 +0100

    pierwszy commit

---
+++ src/main/java/pl/sda/j133/hibernate/fakturownia/Main.java
@@ -0,0 +1,7 @@
+package pl.sda.j133.hibernate.fakturownia;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/sda/j133/hibernate/fakturownia/model/Faktura.java
@@ -0,0 +1,4 @@
+package pl.sda.j133.hibernate.fakturownia.model;
+
+public class Faktura {
+}
